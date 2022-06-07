168658050085ccb3ab302bd2aa446ade16a00c45
ayus <l65.fread@gmail.com>
2022-06-07 15:20:09 +0545

    initial

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,18 @@
+import { FaQuestion } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function AboutIconLink() {
+  return (
+    <div className="about-link">
+      <Link to={{ 
+          pathname: "/about", 
+        //   hash: "#a", 
+        //   search: "?sort=name" 
+          }}>
+        <FaQuestion size={30} />
+      </Link>
+    </div>
+  );
+}
+
+export default AboutIconLink;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function Header({ bgColor, textColor }) {
+  const headerStyle = { backgroundColor: bgColor, color: textColor };
+  return (
+    <header style={headerStyle}>
+      <div className="container">
+        <h2>Feedback UI</h2>
+      </div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.5)",
+  textColor: "#ff6a95",
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
+export default Header;
